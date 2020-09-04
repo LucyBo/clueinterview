61db616dc043bf78baaac164c5ec38d9c0ebb3fb
Lucyna Borkowska <lucyna.borkowska@mail.com>
2020-09-04 17:10:44 +0200

    masonry sketch

--- src/components/Island-1/index.js
+++ src/components/Island-1/index.js
@@ -6,8 +6,8 @@ class IslandI extends Component {
   render() {
     return (
       <div id="island-1">
-        <h2>Island 1 <span>>></span></h2>
-        <div className="container">
+        <h2>Island 1 <span className="arrows">>></span></h2>
+        <div className="container-fluid">
           <div className="row">
             <div className="col col-lg-4 col-md-12 col-sm-12"><Pin/></div>
             <div className="col col-lg-4 col-md-6 col-sm-12"><Pin/></div>
--- src/components/MasonryGallery/index.js
+++ src/components/MasonryGallery/index.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 //import Masonry from 'react-masonry-css';
 import "./MasonryGallery.scss";
+import Pin from "../Pin";
 
 export default class MasonryGallery extends Component {
   render() {
     return (
       <div id="masonry-gallery">
-        Masonry
+        <div class="container-fluid card-columns">
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+          <Pin/>
+        </div>
       </div>
     )
   }
